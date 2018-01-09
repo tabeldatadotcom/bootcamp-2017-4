ca63ba8db76d41c2d45dfbd01db3428b66c559c5
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2018-01-09 13:35:25 +0700

    membuat arraylist

---
+++ src/com/maryanto/dimas/array/ArrayFwList.java
@@ -0,0 +1,24 @@
+package com.maryanto.dimas.array;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.math.BigDecimal;
+
+public class ArrayFwList{
+    
+    public static void main(String[] args){
+        List values = new ArrayList();
+        values.add("Dimas Maryanto");
+        values.add(1);
+        values.add(false);
+        values.add(true);
+        values.add('A');
+        values.add(100f);
+        values.add(0.1d);
+        values.add(new BigDecimal(1000000));
+
+        for(int i = 0; i < values.size() ; i++)
+            System.out.println(values.get(i));
+
+    }
+}
