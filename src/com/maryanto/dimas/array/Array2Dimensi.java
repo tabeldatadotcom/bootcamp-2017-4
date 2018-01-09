a802d2e60a4ae5770293e6a557fb1cbebc17c729
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2018-01-09 11:33:10 +0700

    pointer dinamic array

--- src/com/maryanto/dimas/array/Array2Dimensi.java
+++ src/com/maryanto/dimas/array/Array2Dimensi.java
@@ -2,14 +2,14 @@ package com.maryanto.dimas.array;
 
 public class Array2Dimensi{
     public static void main(String[] args){
-        Integer[][] values = { {1,2,3}, {2,3,5}, {3,4,5} };
+        Integer[][] values = { {1,2,3}, {2,3,5,4}, {3,4,5} };
 
         // menampilkan data pada index ke 0 dan 2
         System.out.println(values[0][2]);
 
         // menampilkan isi yang ada di array 2 dimention
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
+        for(int i = 0; i < values.length; i++){
+            for(int j = 0; j < values[i].length; j++){
                 System.out.print(values[i][j]);
             }
             System.out.println();		
