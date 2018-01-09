9b387e4665d0b3bfb8349e5c5b595d6e5ee0c3cf
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2018-01-09 10:45:09 +0700

    membuat array type primitive

---
+++ src/com/maryanto/dimas/array/PrimitifArray.java
@@ -0,0 +1,17 @@
+package com.maryanto.dimas.array;
+
+public class PrimitifArray{
+
+    public static void main(String[] args){
+        String[] pesertaBootcamp = {
+            "Dimas Maryanto",
+            "Yusuf",
+            "Gian",
+            "Ikbal"
+        };
+
+        System.out.println(
+            "peserta bootcamp index ke 2 adalah "+ pesertaBootcamp[2]
+        );
+    }
+}
