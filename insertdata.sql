44a52b81971aef5d258feb3df142f37da6cad701
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2018-01-16 10:40:38 +0700

    membuat query insert data ke tabel department

---
+++ insertdata.sql
@@ -0,0 +1,7 @@
+-- select * from departments;
+-- select * from employees;
+-- select * from locations;
+-- select * from departments where department_id in (1001, 1002)
+
+insert into departments (department_id, department_name, location_id) values (1002, 'Programming', null);
+
