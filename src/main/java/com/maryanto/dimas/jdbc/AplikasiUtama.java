6b2f0292cfb697e5ed031185ca3e6d70b0c61dbe
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2018-01-18 11:53:05 +0700

    mengambil data dari tabel departments

--- src/main/java/com/maryanto/dimas/jdbc/AplikasiUtama.java
+++ src/main/java/com/maryanto/dimas/jdbc/AplikasiUtama.java
@@ -1,33 +1,51 @@
 package com.maryanto.dimas.jdbc;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 public class AplikasiUtama {
-    
-    public static void koneksi(){
+
+    public static void koneksi() {
         BasicDataSource ds = new BasicDataSource();
         ds.setUsername("hr");
         ds.setPassword("hr");
         ds.setUrl("jdbc:postgresql://localhost:5432/hr");
         ds.setDriverClassName("org.postgresql.Driver");
-        
-        try{
+
+        try {
             // membuka koneksi ke database
             Connection connection = ds.getConnection();
             System.out.println("berhasil koneksi ke database");
-            
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from departments");
+
+//            untuk mengambil data perbaris
+            while (resultSet.next()) {
+                Integer departmentId = resultSet.getInt("department_id");
+                String departmentName = resultSet.getString("department_name");
+                Integer managerId = resultSet.getInt(3);
+
+                System.out.println(
+                        String.format("{ departmentId : %s, departmentName : %s, managerId : %s }",
+                                departmentId, departmentName, managerId));
+            }
+
             // menutup koneksi ke database
+            resultSet.close();
+            statement.close();
             connection.close();
-        }catch(SQLException sqle){
+        } catch (SQLException sqle) {
             System.err.printf("tidak dapat koneksi ke databas!");
         }
     }
 
     public static void main(String[] args) {
         System.out.println("Halo ini aplikasi maven pertama");
-        
+
         koneksi();
     }
 
