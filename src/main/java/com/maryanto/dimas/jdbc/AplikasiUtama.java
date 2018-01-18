8079130bc8254f06b40b417c966c3702d4848f67
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2018-01-18 15:57:58 +0700

    sistem transactional

--- src/main/java/com/maryanto/dimas/jdbc/AplikasiUtama.java
+++ src/main/java/com/maryanto/dimas/jdbc/AplikasiUtama.java
@@ -8,6 +8,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 public class AplikasiUtama {
@@ -19,25 +21,37 @@ public class AplikasiUtama {
         ds.setUrl("jdbc:postgresql://localhost:5432/hr");
         ds.setDriverClassName("org.postgresql.Driver");
 
+        Connection connection = null;
+
         try {
             // membuka koneksi ke database
-            Connection connection = ds.getConnection();
+            connection = ds.getConnection();
+            connection.setAutoCommit(false);
             System.out.println("berhasil koneksi ke database");
             DepartmentDao dao = new DepartmentDao(connection);
-            
+
 //            save nilai department
-            dao.save(new Department(2002, "Sistem Analis", 1000, null));
-            
+            dao.save(new Department(3003, "Sistem Analis", 1000, null));
+            // error karena duplikate primary
+            dao.save(new Department(3004, "Sistem Analis", 1000, null));
+
 //            untuk ambil nilainya
             List<Department> daftarDepartment = dao.findAll();
-            for(Department d : daftarDepartment){
+            for (Department d : daftarDepartment) {
                 System.out.println(d.toString());
             }
-            
+            connection.commit();
             connection.close();
         } catch (SQLException sqle) {
             System.err.printf("tidak dapat koneksi ke databas!");
             sqle.printStackTrace();
+            try {
+                if (connection != null) {
+                    connection.rollback();
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(AplikasiUtama.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
 
