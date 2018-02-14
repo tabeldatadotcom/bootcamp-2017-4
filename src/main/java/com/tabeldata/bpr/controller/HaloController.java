bc5dd5d18ed1a522c6f8e66a42d078d582d2138c
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2018-02-14 14:37:12 +0700

    membuat halaman welcome

---
+++ src/main/java/com/tabeldata/bpr/controller/HaloController.java
@@ -0,0 +1,13 @@
+package com.tabeldata.bpr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HaloController {
+
+    @GetMapping(value = "/index")
+    public String haloController(){
+        return "/welcome";
+    }
+}
