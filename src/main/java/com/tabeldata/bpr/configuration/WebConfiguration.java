568caf7ca4b48cc2663385f5006d64abcfe53601
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2018-02-14 16:04:18 +0700

    load resources

---
+++ src/main/java/com/tabeldata/bpr/configuration/WebConfiguration.java
@@ -0,0 +1,17 @@
+package com.tabeldata.bpr.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        super.addResourceHandlers(registry);
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+}
