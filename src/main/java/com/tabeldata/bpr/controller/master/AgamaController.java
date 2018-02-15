c1060623482a812a27116f9c1c95469fb8864c42
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2018-02-15 17:11:58 +0700

    update membuat form di thymeleaf

--- src/main/java/com/tabeldata/bpr/controller/master/AgamaController.java
+++ src/main/java/com/tabeldata/bpr/controller/master/AgamaController.java
@@ -1,11 +1,17 @@
 package com.tabeldata.bpr.controller.master;
 
+import com.tabeldata.bpr.entity.master.Agama;
 import com.tabeldata.bpr.service.AgamaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/agama")
@@ -16,11 +22,26 @@ public class AgamaController {
 
     /**
      * localhost:8080/agama/ atau localhost:8080/agama/list
+     *
      * @return
      */
     @GetMapping(value = {"/", "/list"})
-    public String listAgama(ModelMap params){
+    public String listAgama(ModelMap params) {
         params.addAttribute("listAgama", agamaService.findAll());
         return "/pages/agama/list";
+    }
+
+    @GetMapping("/form")
+    public String formAgama(Agama agama, ModelMap params) {
+        params.addAttribute("agama", agama);
+        return "/pages/agama/form";
+    }
+
+    @PostMapping("/submit")
+    public String submitAgama(@ModelAttribute Agama agama) {
+        agama.setCreatedDate(Timestamp.valueOf(LocalDateTime.now()));
+        agama.setCreatedBy("admin");
+        agamaService.save(agama);
+        return "redirect:/agama/list";
     }
 }
