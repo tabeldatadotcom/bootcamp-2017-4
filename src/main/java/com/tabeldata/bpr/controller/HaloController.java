51a3346c63ed54c42dbf4220dd5337b5317c9eff
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2018-02-20 14:37:37 +0700

    update layout

--- src/main/java/com/tabeldata/bpr/controller/HaloController.java
+++ src/main/java/com/tabeldata/bpr/controller/HaloController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HaloController {
 
-    @GetMapping(value = "/index")
-    public String haloController(){
+    @GetMapping(value = {"/", "/index"})
+    public String haloController() {
         return "/pages/index";
     }
 }
